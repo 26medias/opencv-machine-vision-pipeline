91c0db106fbb56366ce2cfc3515d7465df682139
26medias <julien@twenty-six-medias.com>
2020-09-12 18:51:30 -0400

    converted into a package, bugfixes, new demos

--- example_events.py
+++ example_events.py
@@ -1,11 +1,21 @@
-from framework import VisionFramework
+from vision_pipeline import framework
 
-from events.examples import onNewObject
-from events.examples import onNewFace
-from events.examples import onNewPerson
-from events.examples import onDeletePerson
+# Some event handlers
+def onNewObject(framework, object_id):
+    print("New object detected!\n", framework.tracker.objects[object_id])
 
-visionPipeline = VisionFramework(settings="settings/gender_age_objects.json")
+def onNewFace(framework, object_id):
+    print("New face detected!\n", framework.tracker.objects[object_id])
+
+def onNewPerson(framework, object_id):
+    print("New person detected!\n", framework.tracker.objects[object_id])
+
+def onDeletePerson(framework, object_id):
+    print("Lost track of this person:\n", framework.tracker.objects[object_id])
+
+
+# Setup the pipeline on 
+visionPipeline = framework.VisionFramework(settings="settings/person_face.json")
 
 visionPipeline.on("object.create", onNewObject)
 visionPipeline.on("object.create", onNewFace, {
